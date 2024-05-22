7246d853113eb1e9e8504e5413cbd005cda17f95
raiyan756 <raiyanmugdho6@gmail.com>
2024-05-22 11:34:42 +0600

    deletation

--- src/modeules/product/product.route.ts
+++ src/modeules/product/product.route.ts
@@ -3,5 +3,8 @@ import { ProductController } from "./product.controller";
 
 const router = express.Router();
 router.post("/", ProductController.AddProducts);
-
+router.get("/", ProductController.getProductById);
+router.get("/:productId", ProductController.getProduct);
+router.put("/:productId", ProductController.updateProductById);
+router.delete("/:productId", ProductController.deleteProductById);
 export const ProductRoute = router;
